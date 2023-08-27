e950af4d8e492c2e64e82d8c6f141149d31f42ce
KforKeepLearning <77017389+kavitasood@users.noreply.github.com>
2023-08-27 12:07:52 -0400

    Inital commit

---
+++ app/src/main/java/com/example/newcalculaterapp/Buttons.kt
@@ -0,0 +1,12 @@
+package com.example.newcalculaterapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Buttons : Fragment(R.layout.fragment_buttons) {
+
+
+}
